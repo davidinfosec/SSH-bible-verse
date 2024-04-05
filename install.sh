8e91fb5cec090857c7e07547d89f6e4a161ec865
David <87215831+davidinfosec@users.noreply.github.com>
2024-04-04 22:57:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,34 @@
 INSTALL_PATH="/opt/SSH-bible-verse"
 REPO_URL="https://github.com/davidinfosec/SSH-bible-verse"
 
-# Install jq
-echo "Installing jq..."
-sudo apt-get install jq || { echo "Failed to install jq."; exit 1; }
+# Install jq if not already installed
+echo "Checking if jq is installed..."
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Installing..."
+    sudo apt-get install jq || { echo "Failed to install jq."; exit 1; }
+else
+    echo "jq is already installed."
+fi
 
-# Step 1: Clone the repository
-echo "Cloning the repository to $INSTALL_PATH..."
-git clone "$REPO_URL" "$INSTALL_PATH" || { echo "Failed to clone the repository."; exit 1; }
+# Check if the installation path exists
+if [ -d "$INSTALL_PATH" ]; then
+    echo "Updating the repository at $INSTALL_PATH..."
+    cd "$INSTALL_PATH" || { echo "Failed to change directory to $INSTALL_PATH"; exit 1; }
+    git pull origin master || { echo "Failed to update the repository."; exit 1; }
+else
+    # Clone the repository if it doesn't exist
+    echo "Cloning the repository to $INSTALL_PATH..."
+    git clone "$REPO_URL" "$INSTALL_PATH" || { echo "Failed to clone the repository."; exit 1; }
+fi
 
 # Change into the directory where the repository has been cloned
 cd "$INSTALL_PATH" || { echo "Failed to change directory to $INSTALL_PATH"; exit 1; }
 
-# Step 3: Move ssh-bible-verse.sh to /etc/profile.d and set execute permissions
+# Move ssh-bible-verse.sh to /etc/profile.d and set execute permissions
 echo "Setting up the ssh-bible-verse script..."
 if [ -f "sshverse.sh" ]; then
     chmod +x "sshverse.sh"
-    mv "sshverse.sh" "/etc/profile.d/sshverse.sh" || { echo "Failed to move and set permissions for sshverse.sh"; exit 1; }
+    sudo mv "sshverse.sh" "/etc/profile.d/sshverse.sh" || { echo "Failed to move and set permissions for sshverse.sh"; exit 1; }
 else
     echo "sshverse.sh not found in the cloned repository."
     exit 1
